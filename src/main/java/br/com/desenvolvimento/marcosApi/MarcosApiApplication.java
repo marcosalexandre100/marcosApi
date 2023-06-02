454c1e082eb6f84af05acd3396308641c3a24b7e
marcosalexandre100 <marcosalexandre100@gmail.com>
2023-06-02 16:23:38 -0300

    Initial commit

---
+++ src/main/java/br/com/desenvolvimento/marcosApi/MarcosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.desenvolvimento.marcosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desenvolvimento/marcosApi/MarcosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desenvolvimento.marcosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
